34e97b4221268be1fcebad1d9292b3c3d9af0fe7
Vp9898 <foade984@outlook.com>
2025-09-30 21:32:01 +0100

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App.jsx' // <--- هذا هو السطر الذي تم تعديله
+import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
